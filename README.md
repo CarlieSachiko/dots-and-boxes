ff82df4570c27fc70c77369567ceaed339a236a8
Carlie Clippinger <carliesachiko@gmail.com>
2016-12-11 17:01:10 -0800

    score keeping works

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 ##Created By Carlie Clippinger
 
 ####About Dots and Boxes
-
+Dots and Boxes is a class board game, usually played with pen and paper. 
 
 
 
 
 
 ####Technologies Used
-
+For thie game I used:
+*HTML
+*CSS
+*Javascript
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,30 +1,4 @@
 /*
-HANGMAN
-[Overall Game Play]
-1. Define the apps vars
-2. Register event listeners
-3. Initialize the apps state
-4. Render display
-5. Handle players clicking theme option (movies, actors, songs, countries, animals, sports, holidays)
-6. Handle players clicking letter on keyboard
-  6a. Do nothing if blanks are full and game is over
-  6b. Make game play move
-    -if not game over check if letter selection matches letter in word
-    -Add letter to array
-    -Check if game is over (if win or lose)
-    -Render display
-*/
-
-/*
-MEMORY PATTERN GAME
-[Overall Game Play]
-1.
-
-
-*/
-
-
-/*
 DOTS & BOXES
 [Overall Game Play]
 1. Define the apps vars
@@ -37,7 +11,7 @@ DOTS & BOXES
   6a. Do nothing if position is full
   6b. Make game play move
     -Check if box is made
-       -if box made add score to player
+       -if box made add score to player and let player move again until no more boxes are made
     -Mark line for current player
     -Render?
     -Check if game is over (if win or lose)
@@ -45,6 +19,98 @@ DOTS & BOXES
     -Render
 */
 
+// VARIABLES //
+var $board, gameOver, $tiedGame, boxA, boxB, boxC, boxD, box;
+var score1=0;
+var score2=0;
+var $score1 = $('div#score1');
+var $score2 = $('div#score2');
+var $message = $('#message');
+var $playAgain = $('#playAgain');
+var $lineH = $('td div.lineH');
+var $lineV = $('td div.lineV');
+var $line1 = $('td div#line1');
+var $line2 = $('td div#line2');
+var $line3 = $('td div#line3');
+var $line4 = $('td div#line4');
+var $line5 = $('td div#line5');
+var $line6 = $('td div#line6');
+var $line7 = $('td div#line7');
+var $line8 = $('td div#line8');
+var $line9 = $('td div#line9');
+var $line10 = $('td div#line10');
+var $line11 = $('td div#line11');
+var $line12 = $('td div#line12');
+var $square = $('td div#square');
+var $squareB = $('td div#squareb');
+var $squareC = $('td div#squarec');
+var $squareD = $('td div#squared');
+
+var player = "#1FE5BB";
+
+// EVENT LISTENERS //
+$lineH.on('click', handleClick);
+
+$lineV.on('click', handleClick);
+
+
+// FUNCTIONS //
+var initialize = function() {
+  player="#1FE5BB";
+  gameOver=false;
+}
+
+var switchPlayer = function(){
+    player = (player==="#1FE5BB") ? "#FF5733" : "#1FE5BB";
+}
+
+var detectBox = function(){
+ if ($line1.data('clicked') && $line3.data('clicked') && $line4.data('clicked') && $line6.data('clicked') && !boxA){
+    $square.css({'background-color':player});
+    boxA=true;
+    box=player;
+    updateScore();
+  } else if ($line2.data('clicked') && $line4.data('clicked') && $line5.data('clicked') && $line7.data('clicked') && !boxB){
+    $squareB.css({'background-color':player});
+    boxB=true;
+    box=player;
+    updateScore();
+  } else if ($line6.data('clicked') && $line8.data('clicked') && $line9.data('clicked') && $line11.data('clicked') && !boxC){
+    $squareC.css({'background-color':player});
+    boxC=true;
+    box=player;
+    updateScore();
+  } else if ($line7.data('clicked') && $line9.data('clicked') && $line10.data('clicked') && $line12.data('clicked') && !boxD){
+    $squareD.css({'background-color':player});
+    boxD=true;
+    box=player;
+    updateScore();
+  } else {
+    return;
+  }
+}
+
+ var updateScore = function(){
+  if (box==="#1FE5BB"){
+    score1++;
+    $score1.html(score1.toString());
+  } else if (box==="#FF5733") {
+    score2++;
+    $score2.html(score2.toString());
+  }
+}
+
+function handleClick(evt) {
+  if(player==="#1FE5BB") {
+    $(this).css({'background-color':player}).data('clicked', true);
+     detectBox();
+     switchPlayer();
+ } else if(player==="#FF5733"){
+    $(this).css({'background-color':player}).data('clicked', true);
+     detectBox();
+     switchPlayer();
+  }
+}
 
 
 
@@ -55,58 +121,3 @@ DOTS & BOXES
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 1. Home screen
-//   1a. Choose theme
-//     -Classic Movies
-//     -Pop Stars
-//     -food
-//     -animals
-// 2. In each theme set game
-//   2a. initialize function
-//     a1. reset display
-//     a2. choose random word from list
-//     a3. show blanks = word.length
-//     a4. show keyboard
-//   2b. on Click keyboard event listener
-//     b1. test missed/matched and win/lose functions
-if clickedLetter = letter in word $$ !gameOver && blanks.length != word.length
-//         -show letter on blank array
-//         -change clickedLetter color
-//         -no longer click clickedLetter
-//         -test missed letter function
-//         -test lose or win
-//           -if hangman is fully shown && blanks.length != word.length print lose message
-//           -if !hangman && blanks.length = word.length print win message and play again or go back to categories options
-
-//     b2. On Click keyboard missed letter function
-//        -if clickedLetter != letter in word && !gameOver
-  //       -change clickedLetter color
-  //       -no longer able to click clickedLetter
-  //       -if !hangman
-  //         -reveal head
-  //         -if head reveal body
-  //           -if body reveal lLeg
-  //             -if lLeg reveal rLeg
-  //               -if both Legs reveal lArm
-  //                 -if lArm reveal rArm and set gameOver = true
-  b3. On click keyboard matched letter function
-    -if clickedLetter = letter in word && !gameOver
-//         -show letter on blank array
-//         -change clickedLetter color
-//         -no longer click clickedLetter
-//           -if hangman is fully shown && blanks.length != word.length print lose message
-//           -if !hangman && blanks.lenght = word.length print win message and play again or go back to categories options
