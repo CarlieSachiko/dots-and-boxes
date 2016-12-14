8bb191f6253e5a480b16c9d607c1784f7fc69d59
Carlie Clippinger <carliesachiko@gmail.com>
2016-12-14 08:51:55 -0800

    score and box fill are working

--- js/main2.js
+++ js/main2.js
@@ -28,7 +28,6 @@ var $lineV = $('td div.v');
 
 // EVENT LISTENERS //
 $line.on('click', handleClick);
-// $lineV.on('click', handleClickV);
 $playAgain.on('click', initialize);
 
 
@@ -49,118 +48,82 @@ function initialize(){
 initialize();
 
 function getAdjCells(lineId){
-  console.log(lineId);
   var lineNum = parseInt(lineId.substr(1));
   var cells=[];
+  var hasTwoCells;
   if(lineId[0]==='h'){
     hArray[lineNum]=player;
-    // for(i=0; i<2; i++){
-      var row=Math.min((Math.floor(lineNum/board[0].length)), board[0].length);
-      var col=Math.min((Math.floor(lineNum-board.length*row)), board[0].length-1);
-      if (row===4){
-        cells.push({row:row-1, col:col});
-      } else if (row===0) {
-        cells.push({row:row, col:col});
-      }else if (row > 0){
-        cells.push({row:row, col:col});
-        cells.push({row:row-1, col:col});
-      }
-    // }
-    console.log(row, col)
-    console.log(hArray);
+    var row = Math.min((Math.floor(lineNum/board[0].length)), board[0].length);
+    hasTwoCells = (row !== board.length && row !== 0);
+    var col = Math.min((Math.floor(lineNum-board.length*row)), board[0].length-1);
+    row -= row === board.length ? 1 : 0;
+    cells.push({row: row, col: col});
+    if (hasTwoCells) cells.push({row: row -1, col: col});
   } else {
     vArray[lineNum]=player;
     var col=Math.min((Math.floor(lineNum/board[0].length)), board[0].length);
+    hasTwoCells = (col !== board.length && col !== 0);
     var row=Math.min((Math.floor(lineNum-board.length*col)), board[0].length-1);
-    if (col===4){
-        cells.push({row:row, col:col-1});
-      } else if (col===0) {
-        cells.push({row:row, col:col});
-      }else if (row > 0){
-        cells.push({row:row, col:col});
-        cells.push({row:row, col:col-1});
-      }
-    console.log(row,col)
-    console.log(vArray);
+    col -= col === board.length ? 1 : 0;
+    cells.push({row: row, col: col});
+    if (hasTwoCells) cells.push({row: row, col: col-1});
   }
-  detectBox(cells);
   console.log(cells);
-}
-
-/* win logic psuedocode
-1. get lineId
-2. get current location of lineId on board
-3. see what is on the
-  - right
-  - left
-  - top
-  - bottom
-4. build object/push to array
-5. check if all corerns of box are occupied
-  - get the current player making the move
-  - if box is "square", award square to current player
-6. if square, award points
-7. check to see if boar is all occupied, which means game over
-*/
-
-function detectBox(cellsArr){
-//if the lines surrounding the cell object are clicked,
-//then a box has been made, push player to board array, and then render
-//get adjacent cells here?
-//getAdjCells(lineId);
-  // var lineNum = parseInt(lineId.substr(1));
-  // for(i=0; i<hArray.length; i++){
-  //   if()
-  // }
-
+  return cells;
 }
 
 function render(){
+  var score1 = 0;
+  var score2 = 0;
 //update box colors, and score depending on board array
- board.forEach(function(box,idx){
-    var $bx = $('#' + idx);
-    if(board[idx]===1){
-      $($bx).css('background-color', p1Color);
-    } else if(board[idx]===2){
-      $($bx).css('background-color', p2Color);
-    } else return;
-  });
-  hArray.forEach(function(box,idx){
+  for(i=0; i<board.length; i++){
+    for(j=0; j<board[i].length; j++){
+      var $bx = $('#' + i + '-' + j);
+      if(board[i][j]===1){
+        $($bx).css('background-color', p1Color);
+        score1++;
+      } else if(board[i][j]===2){
+        $($bx).css('background-color', p2Color);
+        score2++
+      } else {
+        $($bx).css('background-color', 'transparent');
+      }
+      $score1.html(score1.toString());
+      $score2.html(score2.toString());
+    };
+  };
+
+  hArray.forEach(function(ln,idx){
     var $ln = $('#h' + idx);
     if(hArray[idx]===1){
-      $($ln).css('background-color', p1Color);
+      $($ln).css('background-color', p1Color).data('clicked', true);;
     } else if(hArray[idx]===2){
-      $($ln).css('background-color', p2Color);
-    } else return;
+      $($ln).css('background-color', p2Color).data('clicked', true);;
+    } else if(hArray[idx]===0){
+      $($ln).css('background-color', 'transparent');
+    }
   });
-  vArray.forEach(function(box,idx){
+  vArray.forEach(function(ln,idx){
     var $ln = $('#v' + idx);
     if(vArray[idx]===1){
-      $($ln).css('background-color', p1Color);
+      $($ln).css('background-color', p1Color).data('clicked', true);;
     } else if(vArray[idx]===2){
-      $($ln).css('background-color', p2Color);
-    } else return;
+      $($ln).css('background-color', p2Color).data('clicked', true);;
+    } else if(vArray[idx]===0){
+      $($ln).css('background-color', 'transparent');
+    }
   });
-}
-
-function updateScore(){
-//if box has been made, look at board and calculate score
-  if (!boxCreated){
-    return
-  } else {
-    $score1.html(board.filter(function(value){
-        return value === 1;
-      }).length.toString());
-    $score2.httml(board.filter(function(value){
-        return value === 2;
-      }).length.toString());
-  }
+  // updateTurnMessage();
 }
 
 function switchPlayer(){
   //if box has not been made switch players, else cur player gets another turn
   player = (player===1) ? 2 : 1;
-  updateTurnMessage();
+  // if (!lineVals.includes(false)){
+  //   return;
+  // } else {
+  //   player = (player===1) ? 2 : 1;
+  // }
 }
 
 function checkWinner(){
@@ -189,32 +152,30 @@ function updateTurnMessage(){
 
 function handleClick(evt){
    if($(this).data('clicked')){
-    return;
-  } else {
-    if(player===1){
-      $(this).css({'background-color':p1Color}).data('clicked', true);
-    } else {
-      $(this).css({'background-color':p2Color}).data('clicked', true);
-    }
-    getAdjCells($(this).attr('id'));
+      return;
+   }
+    var cells = getAdjCells($(this).attr('id'));
+    cells.forEach(function(cell) {
+      var lineVals = getLineValsForCell(cell);
+      if (!lineVals.includes(false)) {
+        board[cell.row][cell.col] = player;
+        console.log(board);
+      }
+    });
     switchPlayer();
-  }
+    render();
 }
 
-// function handleClickV(evt){
-//    if($(this).data('clicked')){
-//     return;
-//   } else {
-//     if(player===1){
-//       $(this).css({'background-color':p1Color}).data('clicked', true);
-//       vArray[$(this).attr('id').parseInt(substr(1))].push("1");
-//     } else {
-//       $(this).css({'background-color':p2Color}).data('clicked', true);
-//       vArray[$(this).attr('id').parseInt(substr(1))].push("2");
-//     }
-//     detectBox($(this).attr('id'));
-//     switchPlayer();
-//   }
-// }
+function getLineValsForCell(cell) {
+  // var {row, col} = cell; ES2015 for the below two lines
+  var row = cell.row;
+  var col = cell.col;
+  var t = row * board[row].length + col;
+  var b = t + board[row].length;
+  var l = col * board[col].length + row;
+  var r = l + board[col].length;
+  return [hArray[t], vArray[r], hArray[b], vArray[l]];
+}
+
 
 
